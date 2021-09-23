3c24d8ad75e498cd3147fc4b16c24a2d6fb70f9f
Sudman Khan <skhan31@stuy.edu>
2021-09-23 19:46:53 -0400

    Added 2 functions, added main, called 2 functions in main

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,9 @@
-#hello
+public class CtoFTester {
+
+	public static double celsiusToFahrenheit() {}
+	public static double fahrenheitToCelsius() {}
+	public static void main(String[] args) {
+		celsiusToFahrenheit()
+		fahrenheitToCelsius()
+}
+}
