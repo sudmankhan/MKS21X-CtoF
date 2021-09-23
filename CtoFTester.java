3ea13dbbf4eac963dcb964ab6c9002a7e94141e3
Sudman Khan <skhan31@stuy.edu>
2021-09-23 19:54:35 -0400

    Answered questions in comment, fixed some semicolon errors, added temporary return

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,9 +1,16 @@
+// The functions should take a double as a parameter
+// The functions should return a double value  
+
 public class CtoFTester {
 
-	public static double celsiusToFahrenheit() {}
-	public static double fahrenheitToCelsius() {}
+	public static double celsiusToFahrenheit() {
+		return 1.0;
+}
+	public static double fahrenheitToCelsius() {
+		return 1.0;
+}
 	public static void main(String[] args) {
-		celsiusToFahrenheit()
-		fahrenheitToCelsius()
+		celsiusToFahrenheit();
+		fahrenheitToCelsius();
 }
 }
