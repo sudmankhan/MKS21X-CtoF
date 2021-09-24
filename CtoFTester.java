cfa8614f44852ad2dda29a939f34daad9209ad32
Sudman Khan <skhan31@stuy.edu>
2021-09-23 20:09:20 -0400

    fixed computational errors

--- CtoFTester.java
+++ CtoFTester.java
@@ -4,15 +4,15 @@
 public class CtoFTester {
 
 	public static double celsiusToFahrenheit(double x) {
-		return (x * (9/5)) + 32;
+		return (x * 9 / 5) + 32;
 }
 	public static double fahrenheitToCelsius(double y) {
-		return (y - 32.0) * (5/9);
+		return (y - 32.0) * 5 / 9;
 }
 	public static void main(String[] args) {
 		double x = 75;
 		double y = 77;
-		System.out.println("75 C is" + celsiusToFahrenheit(x));
-		System.out.println("30 F is" + fahrenheitToCelsius(y));
+		System.out.println("75 C is " + celsiusToFahrenheit(x));
+		System.out.println("30 F is " + fahrenheitToCelsius(y));
 }
 }
